30b72f72befee1c741d34980baf7a9f4d7d3e550
ShvedovIGD <85112433+ShvedovIGD@users.noreply.github.com>
2021-06-01 11:31:48 +0300

    Update script.js
    Так же можно перенести наш скрипт в отдельный файл  script/script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,36 @@
-
+	function insertNum(num){
+		if(document.inputForm.inputView.value=="")
+			if(num==".")
+				document.inputForm.inputView.value=document.inputForm.inputView.value+"0";
+		if(num=="."&& document.inputForm.inputView.value.includes("."))
+			num="";
+		document.inputForm.inputView.value=document.inputForm.inputView.value+num;
+	}
+	function operation(operator){
+		if(document.inputForm.inputView.value)
+			document.calcForm.calcView.value+=document.inputForm.inputView.value+operator;
+		document.inputForm.inputView.value="";
+	}
+	function clean(){
+		document.inputForm.inputView.value="";
+		document.calcForm.calcView.value="";
+	}
+	function backspace(){
+		var exp=document.inputForm.inputView.value;
+		document.inputForm.inputView.value=exp.substring(0,exp.length-1);
+	}
+	function sign(){
+		var exp=document.inputForm.inputView.value;
+		if(exp){
+			exp=exp+"*-1";
+			document.inputForm.inputView.value = eval(exp);
+		}
+	}
+	function eql(){
+		var exp=document.calcForm.calcView.value+document.inputForm.inputView.value;
+		if(exp){
+			exp=exp.replace("--","+");
+			document.inputForm.inputView.value = Math.round(eval(exp)*1000000000)/1000000000;
+		}
+		document.calcForm.calcView.value="";
+	}
